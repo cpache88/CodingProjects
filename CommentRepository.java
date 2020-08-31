0d7c8899eef62dcf26f7ab33b8ec4670a20134e0
cpache88 <66196733+cpache88@users.noreply.github.com>
2020-08-31 01:10:25 -0700

    Delete CommentRepository.java

--- CommentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.promineotech.socialMediaApi.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.promineotech.socialMediaApi.entity.Comment;
-
-public interface CommentRepository extends CrudRepository<Comment, Long> {
-
-}
