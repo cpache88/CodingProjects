ec0f910e371712e366a86cf5ec589ca5efc67e8c
cpache88 <66196733+cpache88@users.noreply.github.com>
2020-08-31 01:11:18 -0700

    Delete PostRepository.java

--- PostRepository.java
+++
@@ -1,9 +0,0 @@
-package com.promineotech.socialMediaApi.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.promineotech.socialMediaApi.entity.Post;
-
-public interface PostRepository extends CrudRepository<Post, Long>{
-
-}
